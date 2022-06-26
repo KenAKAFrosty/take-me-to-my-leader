6ac1fd92e571b0cbec40bccd5c6c209159902e71
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-26 12:44:10 -0400

    fix: accounting for mobile browser head

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header style={{
             width: "100%",
-            height: 2,
+            height: 3,
             background: `linear-gradient(60deg, #0173f8, #014bf8, #1701f8, #8900f8, #e900f8, #f800df)`,
             boxShadow: "0px 0px 5px #f800df",
             color: "rgba(255,255,255,0.9)",
