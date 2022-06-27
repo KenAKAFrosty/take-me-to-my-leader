3960f8e2c4d28331e9be7d8068fedc7ea94c6261
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-26 21:32:36 -0400

    add: server props, getting geoData

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,57 @@ import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head'
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import getLocationFromIp from '../functions/getLocationFromIp';
+import { getPoliticalLeadersByZip, Official } from '../functions/getPoliticalLeaders';
 import { defaultMainContainer, defaultPageContainer } from '../styles/defaultStyles';
 
+
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const fs = require('fs');
+  const path = require('path');
   const request = context.req;
   const forwarded = request.headers["x-forwarded-for"];
   const ip = forwarded || request.connection.remoteAddress;
-  console.log(ip);
+  const location = await getLocationFromIp(ip);
+
+  // if (!location) { return } // need proper return value still
+  //if no zip on response, see if can use city+state instead
+  //then if not, graceful exit
+
+  const countiesByZipFile = path.join(process.cwd(), "cachedData/countiesByZip.json");
+  const countiesByZip = JSON.parse(fs.readFileSync(countiesByZipFile, 'utf-8'));
+
+  const geoData: GeoData = {
+    city: location?.city,
+    county: location?.country,
+    zip: location?.postal,
+    state: location?.region,
+    stateCode: location?.region_code
+  }
+  if (location?.postal) {
+    geoData.county = countiesByZip[location?.postal];
+  }
 
   return {
-    props: { ip }
+    props: { geoData }
   }
 }
 
+interface GeoData {
+  city?: string;
+  county?: string;
+  zip?: string;
+  state?: string;
+  stateCode?: string;
+}
 
-export default function Home({ ip }: { ip: string }) {
-  console.log('Server says ip is ', ip)
+export default function Home({ geoData }: { leaders: Official[], geoData: GeoData }) {
+  console.log(geoData)
 
   return (
     <div style={defaultPageContainer}>
       <Head>
-        <title>⌚▶📣 Take me to my leader</title>
+        <title>Take me to my leader 📣</title>
         <meta name="description" content="The fastest path to participating in democracy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -43,6 +73,7 @@ export default function Home({ ip }: { ip: string }) {
         }}>
           {`The fastest path to participating in democracy`}
         </h1>
+
 
       </main>
 
