b173c9833d7cfc5cfa89a0a31cdc4dde40a0df3a
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-26 21:32:22 -0400

    add: styles & icons update

--- styles/defaultStyles.ts
+++ styles/defaultStyles.ts
@@ -4,11 +4,12 @@ export const defaultPageContainer: CSSProperties = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
-    minHeight: "99.9vh"
+    minHeight: "100vh"
 }
 
 export const defaultMainContainer: CSSProperties = {
     flexGrow: 1,
     display:"flex",
-    alignItems: "center"
+    justifyContent: "center",
+    flexDirection: "column"
 } 
