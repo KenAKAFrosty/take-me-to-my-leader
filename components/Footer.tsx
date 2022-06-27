88dbafee8044ae9cb6e257a8180d156235b48e89
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-26 21:32:55 -0400

    add: header & footer shells

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,16 @@ export default function Footer() {
             width: "100%",
             height: 2,
             background: `linear-gradient(60deg, #f800df, #ad00f8, #010ff8, #0173f8)`,
-            boxShadow: "0px 0px 5px #0173f8",
+            boxShadow: "0px 0px 3px #0173f8",
             color: "rgba(255,255,255,0.9)",
         }}>
 
         </footer>
     )
 }
+
+/* Need to add this for logo:
+<a href="https://www.flaticon.com/free-icons/pattern" title="pattern icons">
+    Pattern icons created by Us and Up - Flaticon
+</a>
+*/
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
             width: "100%",
             height: 3,
             background: `linear-gradient(60deg, #0173f8, #014bf8, #1701f8, #8900f8, #e900f8, #f800df)`,
-            boxShadow: "0px 0px 5px #f800df",
+            boxShadow: "0px 0px 3px #f800df",
             color: "rgba(255,255,255,0.9)",
         }}>
 
