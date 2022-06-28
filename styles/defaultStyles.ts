eb5f293326dd691e8c65f35fe66dd4c07d7020d2
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-27 22:09:14 -0400

    add: more to default styles

--- styles/defaultStyles.ts
+++ styles/defaultStyles.ts
@@ -4,12 +4,16 @@ export const defaultPageContainer: CSSProperties = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
-    minHeight: "100vh"
+    minHeight: "100vh",
+    width: "100%"
 }
 
 export const defaultMainContainer: CSSProperties = {
     flexGrow: 1,
     display:"flex",
     justifyContent: "center",
-    flexDirection: "column"
+    alignItems: "center",
+    flexDirection: "column",
+    width: "97%",
+    margin: "auto"
 } 
