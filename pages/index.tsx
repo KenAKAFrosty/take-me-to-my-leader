937184d2f02d5a332823e71b089f6d72600a4264
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-29 22:28:17 -0400

    del unneeded imports, fix meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,20 @@ import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head'
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import AddressComponent from '../components/HomePage/AddressComponent';
 import ErrorMessage from '../components/HomePage/ErrorMessage';
-import MessageButton from '../components/HomePage/MessageButton';
-import { NamePortraitAndTitle } from '../components/HomePage/NamePortraitAndTitle';
-import PhoneButton from '../components/HomePage/PhoneButton';
 import LoadingSpinnerWithMessage from '../components/LoadingSpinner';
 import getIndexOfPreferredLeader from '../functions/getIndexOfPreferredLeader';
 import getLocationFromIp, { GeoData } from '../functions/getLocationFromIp';
 import { Official, Leader } from '../functions/getPoliticalLeaders';
 import usePoliticalLeaders from '../functions/hooks/usePoliticalLeaders';
 import { defaultMainContainer, defaultPageContainer } from '../styles/defaultStyles';
-import TwitterBioButton from '../components/TwitterBioButton';
-import Image from 'next/image';
-import dice from "../images/dice.svg"
-import { MouseEvent, MouseEventHandler, useEffect, useState } from 'react';
-import Modal from '../components/Modal';
+import { MouseEvent, useEffect, useState } from 'react';
 import ZipCodeModal from '../components/HomePage/ZipCodeModal';
 import { LeaderCard } from '../components/HomePage/LeaderCard';
 
 const subjectLines = [
   "RE: A concerned Citizen"
 ]
-
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const fs = require('fs');
@@ -106,9 +97,9 @@ export default function Home({ geoData }: { leaders: Official[], geoData: GeoDat
         <title>{title}</title>
         <meta property="og:title" content={slogan} />
         <meta property="og:image" content={`/social-share.png`} />
-        <meta name="description" content={slogan} />
+        <meta name="description" content={title} />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:site_name" content="TakeMeToMyLeader" />
+        <meta property="og:site_name" content={slogan} />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta property="og:locale" content={`en_US`} />
         <meta name="twitter:card" content="summary_large_image" />
