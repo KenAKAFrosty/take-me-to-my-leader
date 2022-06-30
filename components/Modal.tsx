39362240c693f846b51fe84cae2bd055bba1c8d0
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-29 21:19:19 -0400

    add: onBackgroundClick for modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,20 +1,24 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
-export default function Modal({children} : {children: ReactNode}) { 
-    return ( 
-        <section style={{ 
-            position: "absolute",
-            zIndex: 99,
-            top:0,
-            left:0,
-            width:"100vw",
-            height: "100vh",
-            display: "flex",
-            backgroundColor: "rgba(0,0,0,0.90)",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
+export default function Modal({ children, onBackgroundTap }:
+    { children: ReactNode, onBackgroundTap: MouseEventHandler }) {
+
+    return (
+        <section
+            onClick={onBackgroundTap}
+            style={{
+                position: "absolute",
+                zIndex: 99,
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                backgroundColor: "rgba(0,0,0,0.90)",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
             {children}
         </section>
     )
